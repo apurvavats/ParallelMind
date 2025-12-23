f97912229bf3ff2d9cb00780b41794b82de28dd4
Apurva Vats <vatsapurva31@gmail.com>
2025-12-23 22:06:21 +0530

    updated url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,5 +4,5 @@ const isProduction = import.meta.env.MODE === 'production';
 // If we are in production (Vercel), use the Render Backend URL.
 // If we are local, use localhost:5000.
 export const VITE_API_URL = isProduction 
-  ? "https://parallelmind.onrender.com/" // 👈 We will update this later after deploying backend
+  ? "https://parallelmind.onrender.com" // 👈 We will update this later after deploying backend
   : "http://localhost:5000";
